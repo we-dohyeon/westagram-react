d51f1c31fb638ea616800ae616da3e653671f91a
안도현 <andohyeon@andohyeon-ui-MacBookPro.local>
2021-10-28 12:59:10 +0900

    Add: 세션 실습 진행 후

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,97 @@
 import React, { Component } from "react";
 import "./Login.scss";
 
+// 1. 아이디 및 비밀번호 입력
+// 1.1 인풋에 접근 후 해당하는 값을 가져온다.
+// 1.2 값이 조건에 해당되는지 판별
+// 1.3 판별한 결과에 따라서 버튼을 활성화(색상 등 스타일링)
+
+// 2. 로그인 버튼을 클릭
+// 2.1 사용자가 입력한 값이 백엔드로 넘어간다.
+// 2.2 로그인 성공 여부를 받음
+// 2.3 성공 여부에 따라 메인으로 이동
+
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: "",
+      pw: "",
+    };
+  }
+
+  // handleIdInput = (e) => {
+  //   this.setState({
+  //     id: e.target.value,
+  //   });
+  // };
+
+  // hadlePwInput = (e) => {
+  //   this.setState({
+  //     pw: e.target.value,
+  //   });
+  // };
+
+  handleInputs = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  submitLoginInfo = () => {
+    fetch("https://westagram-signup.herokuapp.com/login", {
+      method: "POST",
+      body: { id: this.state.id, password: this.state.pw },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.msg === "SUCCESS") {
+          localStorage.setItem("accessToken", res.token);
+          this.props.history.push("/main");
+        } else {
+          alert("로그인을 다시 시도해주세요!");
+        }
+      });
+  };
+
   render() {
-    return <div>Login</div>;
+    const validation = this.state.id.length > 0 && this.state.pw.length > 0;
+
+    return (
+      <main id="main" className="loginContainer">
+        <div className="loginInner">
+          <h1>Westagram</h1>
+          <form className="loginForm">
+            <input
+              // id="id"
+              name="id"
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+              onChange={this.handleInputs}
+            />
+            <input
+              // id="pw"
+              name="pw"
+              type="password"
+              placeholder="비밀번호"
+              onChange={this.handleInputs}
+            />
+            <button
+              id="loginBtn"
+              type="button"
+              className={validation ? "able" : "disable"}
+              disabled={!validation}
+              onClick={this.submitLoginInfo}
+            >
+              로그인
+            </button>
+          </form>
+          <a href="/">비밀번호를 잊으셨나요?</a>
+        </div>
+      </main>
+    );
   }
 }
 
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,160 @@
 import React, { Component } from "react";
 import "./Main.scss";
 
+// 1. 댓글을 입력한다.
+// 1.1 인풋에 입력된 값을 state에 저장
+
+// 2. 게시 버튼을 누른다.
+// 2.1 저장된 댓글 상태값을 댓글 리스트에 추가
+
+// 3. 입력된 댓글이 댓글 리스트에 추가된다.
+// 3.1 추가된 댓글 리스트를 기반으로 화면 재렌더링
+
 class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      comment: "",
+      commentLists: [],
+    };
+  }
+
+  addComment = (e) => {
+    this.setState({
+      comment: e.target.value,
+    });
+  };
+
+  addCommentLists = () => {
+    // spread operator, 전개구문
+    const { comment, commentLists } = this.state;
+
+    this.setState({
+      commentLists: commentLists.concat(comment),
+      comment: "",
+    });
+  };
+
   render() {
-    return <div>Main</div>;
+    // console.log(this.state.commentLists);
+    return (
+      <>
+        <nav className="nav">
+          <div className="navInner">
+            <div className="titleWrapper">
+              <i className="fab fa-instagram"></i>
+              <span>|</span>
+              <h1 className="title">westagram</h1>
+            </div>
+
+            <div className="searchWrapper">
+              <i className="fas fa-search"></i>
+              <p>검색</p>
+            </div>
+
+            <div>
+              <i className="far fa-compass"></i>
+              <i className="far fa-heart"></i>
+              <i className="far fa-user"></i>
+            </div>
+          </div>
+        </nav>
+
+        <main className="mainContainer">
+          <article>
+            <section className="feed">
+              <div className="feedHeader">
+                <div>
+                  <i className="fas fa-user-circle"></i>
+                  <p>wecode_bootcamp</p>
+                </div>
+                <i className="fas fa-ellipsis-h"></i>
+              </div>
+
+              <div className="feedImg"></div>
+
+              <div className="commentWrapper">
+                <div className="commentIconWrapper">
+                  <div>
+                    <i className="fas fa-heart"></i>
+                    <i className="far fa-comment"></i>
+                    <i className="far fa-envelope-open"></i>
+                  </div>
+                  <i className="far fa-bookmark"></i>
+                </div>
+
+                <div className="commentLike">
+                  <i className="fas fa-user-circle"></i>
+                  <p>
+                    <span className="name">we-dohyeon</span>님 외 10명이
+                    좋아합니다
+                  </p>
+                </div>
+
+                <ul id="commentLists">
+                  <li>
+                    <span className="name">soon_tae</span>
+                    <span>위워크에서 진행한 축구 클래스...</span>
+                  </li>
+                  {this.state.commentLists.map((ele, idx) => {
+                    return (
+                      <li key={idx}>
+                        <span className="name">soon_tae</span>
+                        <span>{ele}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+
+              <div className="comment">
+                <input
+                  id="commentInput"
+                  type="text"
+                  value={this.state.comment}
+                  placeholder="댓글 달기..."
+                  onChange={this.addComment}
+                />
+                <button id="submit" onClick={this.addCommentLists}>
+                  게시
+                </button>
+              </div>
+            </section>
+          </article>
+
+          <aside>
+            <section className="sectionWrapper">
+              <i className="fas fa-user-circle"></i>
+              <div className="sectionInner">
+                <p>wecode_bootcamp</p>
+                <p>Wecode | 위코드</p>
+              </div>
+            </section>
+
+            <div className="contentWrapper">
+              <div className="contentHeader">
+                <p>스토리</p>
+                <p>모두 보기</p>
+              </div>
+              {[1, 2, 3, 4].map((ele, idx) => {
+                return (
+                  <section className="sectionWrapper">
+                    <i className="fas fa-user-circle"></i>
+                    <div className="sectionInner">
+                      <p>wecode_bootcamp</p>
+                      <p>Wecode | 위코드</p>
+                    </div>
+                  </section>
+                );
+              })}
+            </div>
+
+            <section className="recommendationWrapper"></section>
+          </aside>
+        </main>
+      </>
+    );
   }
 }
 
